40a198b5855f9f6c75ca197d212373acf9c9f93c
кирилл <kiril.novikov.2222@mail.ru>
2021-07-30 00:01:59 +0300

    added Flyway

---
+++ web/src/main/resources/migration/V1_2_initial_data.sql
@@ -0,0 +1,39 @@
+
+INSERT INTO public.roles (id, role_name) VALUES (2, 'ROLE_ADMIN');
+INSERT INTO public.roles (id, role_name) VALUES (1, 'ROLE_USER');
+
+INSERT INTO public.rooms (id, name, price, principle_of_placement, comfort_level, number_room, rating_average) VALUES (1, 'dfdf', 123, 'lux', '1', 123, 2);
+INSERT INTO public.rooms (id, name, price, principle_of_placement, comfort_level, number_room, rating_average) VALUES (3, '123123', 123, 'lll', '1', 123, 2);
+
+
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (21, '12345', '12345', '22', '12345', false, false, '2021-06-19 21:00:00.000000', '2021-06-19 21:00:00.000000', 1, '2021-06-19 21:00:00.000000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (22, '12345', '12345', '33', '12345', true, false, '2021-06-19 21:00:00.000000', '2021-06-19 21:00:00.000000', 1, '2021-06-19 21:00:00.000000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (20, '12345', '12345', '12345', '12345', false, true, '2021-06-19 21:00:00.000000', '2021-06-19 21:00:00.000000', 1, '2021-06-19 21:00:00.000000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (5, 'wegbq', '213sf', 'aaa', '111', true, true, '2021-06-10 21:00:00.000000', '2021-06-10 21:00:00.000000', 12, null);
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (4, 'wegbq', '213sf', 'AJSFKAJ@mail.ru', '1423', true, true, '2021-06-10 21:00:00.000000', '2021-06-10 21:00:00.000000', 12, null);
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (23, '111', '1223', '111', '1111', false, false, '2021-07-13 13:02:11.873000', '2021-07-13 13:02:11.873000', 1, '2021-07-13 13:02:11.873000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (24, '111', '1223', '111', '1111', false, false, '2021-07-13 13:04:37.666000', '2021-07-13 13:04:37.666000', 1, '2021-07-13 13:04:37.666000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (25, '111', '1223', '111', '1111', false, false, '2021-07-13 13:07:19.403000', '2021-07-13 13:07:19.403000', 1, '2021-07-13 13:07:19.403000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (26, '111', '1223', '111', '1111', false, false, '2021-07-13 13:07:50.499000', '2021-07-13 13:07:50.499000', 1, '2021-07-13 13:07:50.499000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (27, '111', '1223', '111', '1111', false, false, '2021-07-13 13:08:09.502000', '2021-07-13 13:08:09.502000', 1, '2021-07-13 13:08:09.502000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (28, '111', '1223', '111', '1111', false, false, '2021-07-13 13:11:50.291000', '2021-07-13 13:11:50.291000', 1, '2021-07-13 13:11:50.291000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (29, '111', '1223', '111', '1111', false, false, '2021-07-13 13:12:06.944000', '2021-07-13 13:12:06.944000', 1, '2021-07-13 13:12:06.944000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (30, '111', '1223', '111', '1111', false, false, '2021-07-13 13:13:32.830000', '2021-07-13 13:13:32.830000', 1, '2021-07-13 13:13:32.830000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (31, '111', '1223', '111', '1111', false, false, '2021-07-13 13:14:56.013000', '2021-07-13 13:14:56.013000', 1, '2021-07-13 13:14:56.013000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (32, '111', '1223', '111', '1111', false, false, '2021-07-13 13:16:38.407000', '2021-07-13 13:16:38.407000', 1, '2021-07-13 13:16:38.407000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (33, '111', '1223', '111', '1111', false, false, '2021-07-13 13:17:32.993000', '2021-07-13 13:17:32.993000', 1, '2021-07-13 13:17:32.993000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (34, '111', '1223', '111', '1111', false, false, '2021-07-13 13:22:46.424000', '2021-07-13 13:22:46.424000', 1, '2021-07-13 13:22:46.424000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (35, '111', '1223', '111', '1111', false, false, '2021-07-13 13:24:50.855000', '2021-07-13 13:24:50.855000', 1, '2021-07-13 13:24:50.855000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (36, '111', '1223', '111', '1111', false, false, '2021-07-13 13:26:35.120000', '2021-07-13 13:26:35.120000', 1, '2021-07-13 13:26:35.120000');
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (38, 'kirill', 'novikov', 'kiril@mail.ru', '12345', false, false, '1970-01-01 00:00:10.000000', '1970-01-01 00:00:10.000000', 5, '1970-01-01 00:00:10.000000');
+
+
+
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (1, 1, 2);
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (2, 3, 1);
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (3, 1, 1);
+
+
+INSERT INTO public.orders (id, id_room, data_check_in, data_check_out, status, id_user, created, changed, general_price, rating_for_room, rating_for_client) VALUES (9, 1, '2021-06-09 21:00:00.000000', '2021-06-16 21:00:00.000000', 'ds', 4, '2021-06-16 21:00:00.000000', '2021-06-13 21:00:00.000000', 123, 123, null);
+INSERT INTO public.orders (id, id_room, data_check_in, data_check_out, status, id_user, created, changed, general_price, rating_for_room, rating_for_client) VALUES (10, 1, '2021-06-17 21:00:00.000000', '2021-06-19 21:00:00.000000', 'ds', 5, '2021-06-19 21:00:00.000000', '2021-06-14 21:00:00.000000', 123, 123, null);
+INSERT INTO public.orders (id, id_room, data_check_in, data_check_out, status, id_user, created, changed, general_price, rating_for_room, rating_for_client) VALUES (11, 1, '2021-06-20 21:00:00.000000', '2021-06-22 21:00:00.000000', 'ds', 4, '2021-06-26 21:00:00.000000', '2021-06-17 21:00:00.000000', 123, 123, null);
