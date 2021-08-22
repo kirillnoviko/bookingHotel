1949814aedc4a2bc237d097febcd1ca872ef69f7
кирилл <kiril.novikov.2222@mail.ru>
2021-08-22 23:28:01 +0300

    fix flyway(edited data in scripts)

--- common/src/main/resources/migration/V1_1__initial_script.sql
+++ common/src/main/resources/migration/V1_1__initial_script.sql
@@ -1,154 +1,135 @@
-create table users
+create table if not exists users
 (
-    id             bigserial                not null
+    id bigserial not null
         constraint users_pkey
             primary key,
-    name           varchar(200)             not null,
-    surname        varchar(200)             not null,
-    gmail          varchar(200)             not null,
-    password       varchar(50)              not null,
-    is_deleted     boolean                  not null,
-    is_banned      boolean                  not null,
-    created        timestamp with time zone not null,
-    changed        timestamp with time zone not null,
+    name varchar(200) not null,
+    surname varchar(200) not null,
+    gmail varchar(200) not null,
+    password varchar(50) not null,
+    is_deleted boolean default false not null,
+    is_banned boolean default false not null,
+    created timestamp with time zone not null,
+    changed timestamp with time zone not null,
     rating_average bigint,
-    birth_date     timestamp with time zone
+    birth_date timestamp with time zone
 );
 
-alter table users
-    owner to postgres;
+alter table users owner to postgres;
 
-create table rooms
+create unique index if not exists users_gmail_uindex
+    on users (gmail);
+
+create table if not exists rooms
 (
-    id                     bigint  not null
+    id bigserial not null
         constraint section_hotel_room_pkey
             primary key,
-    name                   varchar(200)                                     not null,
-    price                  bigint                                           not null,
-    principle_of_placement varchar(200)                                     not null,
-    comfort_level          varchar(200)                                     not null,
-    number_room            bigint                                           not null,
-    rating_average         bigint                                           not null,
+    name varchar(200) not null,
+    price bigint not null,
+    principle_of_placement varchar(200) not null,
+    comfort_level varchar(200) not null,
+    number_room bigint not null,
+    rating_average bigint not null,
+    is_deleted boolean default false,
     constraint section_hotel_room_name_price_principle_of_placement_comfor_key
         unique (name, price, principle_of_placement, comfort_level)
 );
 
-alter table rooms
-    owner to postgres;
+alter table rooms owner to postgres;
 
-create table comforts
+create table if not exists comforts
 (
-    id           bigint not null
+    id bigserial not null
         constraint comfort_pkey
             primary key,
-    name_comfort varchar(100)                                        not null,
+    name_comfort varchar(100) not null,
     constraint comfort_name_additional_id_key
         unique (name_comfort, id)
 );
 
-alter table comforts
-    owner to postgres;
+alter table comforts owner to postgres;
 
-create table comforts_rooms
+create table if not exists comforts_rooms
 (
-    id         bigint  not null
+    id bigserial not null
         constraint additional_in_section_pkey
             primary key,
-    id_room    bigint                                                    not null
+    id_room bigint not null
         constraint additional_in_section_id_section_hotel_room_fkey
             references rooms,
-    id_comfort bigint                                                    not null
+    id_comfort bigint not null
         constraint additional_in_section_id_additional_comfort_fkey
             references comforts,
     constraint additional_in_section_id_section_hotel_room_id_additional_c_key
         unique (id_room, id_comfort)
 );
 
-alter table comforts_rooms
-    owner to postgres;
+alter table comforts_rooms owner to postgres;
 
-create table orders
+create table if not exists orders
 (
-    id                bigserial                not null
+    id bigserial not null
         constraint orders_pkey
             primary key,
-    id_room           bigint                   not null
+    id_room bigint not null
         constraint orders_id_room_fkey
             references rooms,
-    data_check_in     timestamp with time zone not null,
-    data_check_out    timestamp with time zone not null,
-    status            varchar(50)              not null,
-    id_user           bigint                   not null
+    data_check_in timestamp with time zone not null,
+    data_check_out timestamp with time zone not null,
+    status varchar(50) not null,
+    id_user bigint not null
         constraint orders_id_user_fkey
             references users,
-    created           timestamp with time zone not null,
-    changed           timestamp with time zone not null,
-    general_price     bigint                   not null,
-    rating_for_room   bigint,
+    created timestamp with time zone not null,
+    changed timestamp with time zone not null,
+    rating_for_room bigint,
     rating_for_client bigint,
-    constraint orders_id_id_hotel_room_data_check_in_data_check_out_statu_key
-        unique (id, id_room, data_check_in, data_check_out, status, id_user, general_price),
     constraint orders_created_changed_key
         unique (created, changed)
 );
 
-alter table orders
-    owner to postgres;
+alter table orders owner to postgres;
 
-create table roles
+create table if not exists roles
 (
-    id        bigint  not null
+    id bigserial not null
         constraint system_pkey
             primary key,
-    role_name varchar(100)                                     not null
+    role_name varchar(100) not null
 );
 
-alter table roles
-    owner to postgres;
+alter table roles owner to postgres;
 
-create table sales
+create table if not exists user_roles
 (
-    id           bigserial    not null
-        constraint sales_pkey
-            primary key,
-    name_sale    varchar(100) not null,
-    percent_sale bigint       not null,
-    constraint sales_id_name_percent_sale_key
-        unique (id, name_sale, percent_sale)
-);
-
-alter table sales
-    owner to postgres;
-
-create table user_roles
-(
-    id      bigint not null
+    id bigserial not null
         constraint user_roles_pkey
             primary key,
-    user_id bigint                                                not null
+    user_id bigint not null
         constraint user_roles_user_id_fkey
             references users,
-    role_id bigint                                                not null
+    role_id bigint not null
         constraint user_roles_role_id_fkey
             references roles
 );
 
-alter table user_roles
-    owner to postgres;
+alter table user_roles owner to postgres;
 
-create table sales_orders
+create table if not exists uuid_key_registration
 (
-    id       bigint not null
-        constraint sales_orders_pkey
+    id serial not null
+        constraint table_name_pk
             primary key,
-    id_order bigint                                                  not null
-        constraint sales_orders_id_order_fkey
-            references orders,
-    id_sale  bigint                                                  not null
-        constraint sales_orders_id_sale_fkey
-            references sales
+    uuid varchar,
+    created timestamp not null,
+    id_user bigint not null
+        constraint uuid_key_registration_users_id_fk
+            references users
 );
 
-alter table sales_orders
-    owner to postgres;
+alter table uuid_key_registration owner to postgres;
+
+create unique index if not exists table_name_id_uindex
+    on uuid_key_registration (id);
 
--- common/src/main/resources/migration/V1_2__initial_data.sql
+++ common/src/main/resources/migration/V1_2__initial_data.sql
@@ -1,40 +1,63 @@
 
+
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (16, 'kirill1', '1', 'kiril.1@gmail.com', '12345', false, false, '2021-08-22 18:48:15.572000', '2021-08-22 18:48:15.572000', null, null);
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (17, 'kirill2', '2', 'kiril.2@gmail.com', '12345', false, false, '2021-08-22 18:48:15.572000', '2021-08-22 18:48:15.572000', null, null);
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (18, 'kirill3', '3', 'kiril.3@gmail.com', '12345', false, false, '2021-08-22 18:48:15.572000', '2021-08-22 18:48:15.572000', null, null);
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (19, 'kirill4', '4', 'kiril.4@gmail.com', '12345', false, false, '2021-08-22 18:48:15.572000', '2021-08-22 18:48:15.572000', null, null);
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (20, 'kirill5', '5', 'kiril.5@gmail.com', '12345', false, false, '2021-08-22 18:48:15.572000', '2021-08-22 18:48:15.572000', null, null);
+INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (21, 'kirill6', '6', 'kiril.6@gmail.com', '12345', false, false, '2021-08-22 18:48:15.572000', '2021-08-22 18:48:15.572000', null, null);
+
+
+
 INSERT INTO public.roles (id, role_name) VALUES (2, 'ROLE_ADMIN');
 INSERT INTO public.roles (id, role_name) VALUES (1, 'ROLE_USER');
 
-INSERT INTO public.rooms (id, name, price, principle_of_placement, comfort_level, number_room, rating_average) VALUES (1, 'dfdf', 123, 'lux', '1', 123, 2);
-INSERT INTO public.rooms (id, name, price, principle_of_placement, comfort_level, number_room, rating_average) VALUES (3, '123123', 123, 'lll', '1', 123, 2);
+INSERT INTO public.rooms (id, name, price, principle_of_placement, comfort_level, number_room, rating_average, is_deleted) VALUES (4, 'apartments', 123, 'single', 'standard', 12, 2, false);
+INSERT INTO public.rooms (id, name, price, principle_of_placement, comfort_level, number_room, rating_average, is_deleted) VALUES (1, 'room', 123, 'single', 'lux', 13, 2, false);
+INSERT INTO public.rooms (id, name, price, principle_of_placement, comfort_level, number_room, rating_average, is_deleted) VALUES (2, 'familyLarge', 122, 'triple', 'standard', 15, 5, false);
+INSERT INTO public.rooms (id, name, price, principle_of_placement, comfort_level, number_room, rating_average, is_deleted) VALUES (3, 'familyLux', 120, 'double', 'lux', 14, 2, false);
 
 
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (21, '12345', '12345', '22', '12345', false, false, '2021-06-19 21:00:00.000000', '2021-06-19 21:00:00.000000', 1, '2021-06-19 21:00:00.000000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (22, '12345', '12345', '33', '12345', true, false, '2021-06-19 21:00:00.000000', '2021-06-19 21:00:00.000000', 1, '2021-06-19 21:00:00.000000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (20, '12345', '12345', '12345', '12345', false, true, '2021-06-19 21:00:00.000000', '2021-06-19 21:00:00.000000', 1, '2021-06-19 21:00:00.000000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (5, 'wegbq', '213sf', 'aaa', '111', true, true, '2021-06-10 21:00:00.000000', '2021-06-10 21:00:00.000000', 12, null);
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (4, 'wegbq', '213sf', 'AJSFKAJ@mail.ru', '1423', true, true, '2021-06-10 21:00:00.000000', '2021-06-10 21:00:00.000000', 12, null);
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (23, '111', '1223', '111', '1111', false, false, '2021-07-13 13:02:11.873000', '2021-07-13 13:02:11.873000', 1, '2021-07-13 13:02:11.873000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (24, '111', '1223', '111', '1111', false, false, '2021-07-13 13:04:37.666000', '2021-07-13 13:04:37.666000', 1, '2021-07-13 13:04:37.666000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (25, '111', '1223', '111', '1111', false, false, '2021-07-13 13:07:19.403000', '2021-07-13 13:07:19.403000', 1, '2021-07-13 13:07:19.403000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (26, '111', '1223', '111', '1111', false, false, '2021-07-13 13:07:50.499000', '2021-07-13 13:07:50.499000', 1, '2021-07-13 13:07:50.499000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (27, '111', '1223', '111', '1111', false, false, '2021-07-13 13:08:09.502000', '2021-07-13 13:08:09.502000', 1, '2021-07-13 13:08:09.502000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (28, '111', '1223', '111', '1111', false, false, '2021-07-13 13:11:50.291000', '2021-07-13 13:11:50.291000', 1, '2021-07-13 13:11:50.291000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (29, '111', '1223', '111', '1111', false, false, '2021-07-13 13:12:06.944000', '2021-07-13 13:12:06.944000', 1, '2021-07-13 13:12:06.944000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (30, '111', '1223', '111', '1111', false, false, '2021-07-13 13:13:32.830000', '2021-07-13 13:13:32.830000', 1, '2021-07-13 13:13:32.830000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (31, '111', '1223', '111', '1111', false, false, '2021-07-13 13:14:56.013000', '2021-07-13 13:14:56.013000', 1, '2021-07-13 13:14:56.013000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (32, '111', '1223', '111', '1111', false, false, '2021-07-13 13:16:38.407000', '2021-07-13 13:16:38.407000', 1, '2021-07-13 13:16:38.407000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (33, '111', '1223', '111', '1111', false, false, '2021-07-13 13:17:32.993000', '2021-07-13 13:17:32.993000', 1, '2021-07-13 13:17:32.993000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (34, '111', '1223', '111', '1111', false, false, '2021-07-13 13:22:46.424000', '2021-07-13 13:22:46.424000', 1, '2021-07-13 13:22:46.424000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (35, '111', '1223', '111', '1111', false, false, '2021-07-13 13:24:50.855000', '2021-07-13 13:24:50.855000', 1, '2021-07-13 13:24:50.855000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (36, '111', '1223', '111', '1111', false, false, '2021-07-13 13:26:35.120000', '2021-07-13 13:26:35.120000', 1, '2021-07-13 13:26:35.120000');
-INSERT INTO public.users (id, name, surname, gmail, password, is_deleted, is_banned, created, changed, rating_average, birth_date) VALUES (38, 'kirill', 'novikov', 'kiril@mail.ru', '12345', false, false, '1970-01-01 00:00:10.000000', '1970-01-01 00:00:10.000000', 5, '1970-01-01 00:00:10.000000');
 
-INSERT INTO public.comforts (id, name_comfort) VALUES (1, 'kondition');
 INSERT INTO public.comforts (id, name_comfort) VALUES (2, 'wi-fi');
+INSERT INTO public.comforts (id, name_comfort) VALUES (1, 'bed');
+INSERT INTO public.comforts (id, name_comfort) VALUES (3, 'condition');
+INSERT INTO public.comforts (id, name_comfort) VALUES (4, 'TV');
+INSERT INTO public.comforts (id, name_comfort) VALUES (5, 'balcony');
 
-INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (1, 1, 2);
-INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (2, 3, 1);
-INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (3, 1, 1);
+
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (8, 1, 1);
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (9, 2, 1);
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (10, 3, 1);
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (11, 4, 1);
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (12, 1, 3);
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (13, 1, 2);
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (14, 2, 3);
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (15, 2, 2);
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (16, 2, 5);
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (17, 3, 4);
+INSERT INTO public.comforts_rooms (id, id_room, id_comfort) VALUES (18, 2, 4);
 
 
-INSERT INTO public.orders (id, id_room, data_check_in, data_check_out, status, id_user, created, changed, general_price, rating_for_room, rating_for_client) VALUES (9, 1, '2021-06-09 21:00:00.000000', '2021-06-16 21:00:00.000000', 'ds', 4, '2021-06-16 21:00:00.000000', '2021-06-13 21:00:00.000000', 123, 123, null);
-INSERT INTO public.orders (id, id_room, data_check_in, data_check_out, status, id_user, created, changed, general_price, rating_for_room, rating_for_client) VALUES (10, 1, '2021-06-17 21:00:00.000000', '2021-06-19 21:00:00.000000', 'ds', 5, '2021-06-19 21:00:00.000000', '2021-06-14 21:00:00.000000', 123, 123, null);
-INSERT INTO public.orders (id, id_room, data_check_in, data_check_out, status, id_user, created, changed, general_price, rating_for_room, rating_for_client) VALUES (11, 1, '2021-06-20 21:00:00.000000', '2021-06-22 21:00:00.000000', 'ds', 4, '2021-06-26 21:00:00.000000', '2021-06-17 21:00:00.000000', 123, 123, null);
+INSERT INTO public.orders (id, id_room, data_check_in, data_check_out, status, id_user, created, changed, rating_for_room, rating_for_client) VALUES (35, 1, '2021-08-22 23:12:22.493000', '2021-08-24 23:12:25.005000', 'BOOKED', 16, '2021-08-22 23:12:48.351000', '2021-08-22 23:12:50.756000', null, null);
+INSERT INTO public.orders (id, id_room, data_check_in, data_check_out, status, id_user, created, changed, rating_for_room, rating_for_client) VALUES (36, 1, '2021-07-22 23:12:22.493000', '2021-07-22 23:12:25.005000', 'BOOKED', 16, '2021-08-22 23:12:48.351000', '2021-08-22 23:15:50.756000', null, null);
+INSERT INTO public.orders (id, id_room, data_check_in, data_check_out, status, id_user, created, changed, rating_for_room, rating_for_client) VALUES (37, 1, '2021-07-24 23:12:22.493000', '2021-07-27 23:12:25.005000', 'CANCELED', 16, '2021-08-22 23:12:48.351000', '2021-08-22 23:10:50.756000', null, null);
+INSERT INTO public.orders (id, id_room, data_check_in, data_check_out, status, id_user, created, changed, rating_for_room, rating_for_client) VALUES (38, 2, '2021-08-22 23:12:22.493000', '2021-08-25 23:12:25.005000', 'BOOKED', 16, '2021-08-22 23:12:48.351000', '2021-08-22 23:19:50.756000', null, null);
+INSERT INTO public.orders (id, id_room, data_check_in, data_check_out, status, id_user, created, changed, rating_for_room, rating_for_client) VALUES (39, 3, '2021-08-22 23:12:22.493000', '2021-08-22 23:12:25.005000', 'BOOKED', 16, '2021-08-22 23:12:48.351000', '2021-08-22 22:11:50.756000', null, null);
+
+
+
+
+
+
+INSERT INTO public.user_roles (id, user_id, role_id) VALUES (32, 16, 1);
+INSERT INTO public.user_roles (id, user_id, role_id) VALUES (33, 17, 1);
+INSERT INTO public.user_roles (id, user_id, role_id) VALUES (34, 18, 1);
+INSERT INTO public.user_roles (id, user_id, role_id) VALUES (35, 19, 1);
+INSERT INTO public.user_roles (id, user_id, role_id) VALUES (36, 20, 1);
+INSERT INTO public.user_roles (id, user_id, role_id) VALUES (37, 21, 1);
+INSERT INTO public.user_roles (id, user_id, role_id) VALUES (38, 19, 2);
+INSERT INTO public.user_roles (id, user_id, role_id) VALUES (39, 18, 2);
+INSERT INTO public.user_roles (id, user_id, role_id) VALUES (40, 17, 2);
+
+
